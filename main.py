5c5920b9c91432f006b222bef9ea20fde5953f98
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 <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>
2022-08-20 19:01:29 +0200

    Adding Jupyter Notebook

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+#%%
 def name_as_uppercase(name):
     return name.upper()
 
+name_as_uppercase('amina koyayim')
 
+aa = np.array([1, 2, 3, 4, 5])
+bb = aa**2
+
+plt.scatter(aa, bb)
+plt.show()
+
+#%%
 def check_balance(amount_paid, loan):
     return amount_paid - loan
 
